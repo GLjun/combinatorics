3a1a2a3eb13ea34c141920089c26d342948c8c08
cgn <sdjzgw@163.com>
2017-11-06 10:00:07 +0800

    bug

--- permutation/permutation_generater.cpp
+++ permutation/permutation_generater.cpp
@@ -82,14 +82,14 @@ void test_algorithm()
 	 * generator_library 是字典序版本，支持任意长度的N
 	 * generator_incremental_nolimit和generator_incremental 是递增进制版本，其中nolimit	版本直接对递增进制数组进行加1操作，因而支持任意序列长度N，而后者是先用uint64的数加1，然后计算出递增进制数，因而有上限
 	 * generator_degressive_nolimit和generator_incremental 是递减进制版本，其中nolimit	版本直接对递减进制数组进行加1操作，因而支持任意序列长度N，而后者是先用uint64的数加1，然后计算出递减进制数，因而有上限
-	 * generator_neighbour_nolimit和generator_neighbour 是邻位对换版本，其中nolimit 版本直接对递减进制数组进行加1操作，因而支持任意序列长度N，而后者是先用uint64的数加1，然后计算出递减进制数，因而有上限
+	 * generator_neighbour_nolimit和generator_neighbour 是邻位对换版本，其中nolimit 版本直接对递减进制数组进行加1操作，因而支持任意序列长度N，而后者是先用uint64的数加1，然后计算出递减进制数，因而有上限*/
 	//generator_library(8);
 	//generator_incremental(8);
 	generator_incremental_nolimit(4);
 	//generator_degressive(4);
 	//generator_degressive_nolimit(15);
 	//generator_neighbour(4);
-	//generator_neighbour_nolimit(4);*/
+	//generator_neighbour_nolimit(4);
 }
 
 
